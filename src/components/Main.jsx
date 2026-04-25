698925ccbc7cad90be6e25cc25e1d0e93d85447d
vinothrobinson <vinothrobinson03@gmail.com>
2026-04-25 15:23:39 -0400

    Adding an ingredients list and mapping from form inputs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,27 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const listIngredients = ingredients.map((item) => <li key={item}>{item}</li>);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
+          name="ingredient"
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add Ingredient"
         />
         <button>Add Ingredient</button>
       </form>
+      <ul>{listIngredients}</ul>
     </main>
   );
 }
