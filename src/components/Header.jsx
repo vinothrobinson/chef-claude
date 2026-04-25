a0a7c46cf4bb655ec41a9869805f282267c16e47
vinothrobinson <vinothrobinson03@gmail.com>
2026-04-25 14:21:07 -0400

    Created the header of the page

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import chef from "../assets/chef-claude.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={chef} alt="chef claude logo" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
