b211f3d2417ed041215fd8ded488580b89105dc0
vinothrobinson <vinothrobinson03@gmail.com>
2026-04-29 15:19:07 -0400

    Refactored the code into different components

---
+++ src/components/IngredientsList.jsx
@@ -0,0 +1,22 @@
+export default function IngredientList(props) {
+  const listIngredients = props.ingredients.map((item) => (
+    <li key={item}>{item}</li>
+  ));
+  return (
+    <section>
+      <h2>Ingredients on hand:</h2>
+      <ul className="ingredients-list" aria-live="polite">
+        {listIngredients}
+      </ul>
+      {listIngredients.length >= 4 && (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={props.showRecipe}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  );
+}
