f29af98230a57150589ec24b9e4cde3fa705bcf8
vinothrobinson <vinothrobinson03@gmail.com>
2026-04-30 15:53:04 -0400

    Formatted the received markdown output into something that React can render better

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,10 +1,10 @@
+import ReactMarkdown from "react-markdown";
+
 export default function ClaudeRecipe(props) {
   return (
-    <section>
+    <section className="suggested-recipe-container" aria-live="polite">
       <h2>Chef Claude Recommends:</h2>
-      <article className="suggested-recipe-container" aria-live="polite">
-        {props.recipe}
-      </article>
+      <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </section>
   );
 }
